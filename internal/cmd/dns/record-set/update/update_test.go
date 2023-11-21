8af774fdbb3b99b278820c89a261c79936481549
João Palet <joao.palet@outlook.com>
2023-11-21 16:29:43 +0000

    Disallow updated not trying to set any fields

--- internal/cmd/dns/record-set/update/update_test.go
+++ internal/cmd/dns/record-set/update/update_test.go
@@ -91,13 +91,13 @@ func TestParseFlags(t *testing.T) {
 			isValid:     false,
 		},
 		{
-			description: "required fields only",
+			description: "required flags only (no values to update)",
 			flagValues: map[string]string{
 				projectIdFlag:   testProjectId,
 				zoneIdFlag:      testZoneId,
 				recordSetIdFlag: testRecordSetId,
 			},
-			isValid: true,
+			isValid: false,
 			expectedModel: &flagModel{
 				ProjectId:   testProjectId,
 				ZoneId:      testZoneId,
