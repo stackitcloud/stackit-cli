d363562c3a273e1ba923ba140a3134bf2dd10904
Henrique Santos <henrique.santos@freiheit.com>
2023-11-22 10:58:18 +0000

    Implement ske enable

--- internal/cmd/ske/ske.go
+++ internal/cmd/ske/ske.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stackitcloud/stackit-cli/internal/cmd/ske/cluster"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/ske/describe"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/ske/enable"
 
 	"github.com/spf13/cobra"
 )
@@ -22,5 +23,6 @@ func NewCmd() *cobra.Command {
 
 func addSubcommands(cmd *cobra.Command) {
 	cmd.AddCommand(describe.NewCmd())
+	cmd.AddCommand(enable.NewCmd())
 	cmd.AddCommand(cluster.NewCmd())
 }
