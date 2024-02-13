fb0cc8630110784016ef3ae7273a567f21443264
João Palet <joao.palet@freiheit.com>
2024-02-13 16:06:29 +0000

    Allow titles (draft)

--- internal/pkg/tables/tables.go
+++ internal/pkg/tables/tables.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stackitcloud/stackit-cli/internal/pkg/pager"
 
 	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/spf13/cobra"
 )
 
@@ -19,6 +20,11 @@ func NewTable() Table {
 	return Table{
 		table: t,
 	}
+}
+
+// Sets the title of the table
+func (t *Table) SetTitle(title string) {
+	t.table.SetTitle(title)
 }
 
 // Sets the header of the table
@@ -48,6 +54,11 @@ func (t *Table) EnableAutoMergeOnColumns(columns ...int) {
 // Returns the table rendered
 func (t *Table) Render() string {
 	t.table.SetStyle(table.StyleLight)
+
+	t.table.Style().Title = table.TitleOptionsBlackOnCyan
+	t.table.Style().Title.Format = text.FormatTitle
+	t.table.Style().Title.Align = text.AlignCenter
+
 	t.table.Style().Options.DrawBorder = false
 	t.table.Style().Options.SeparateRows = false
 	t.table.Style().Options.SeparateColumns = true
