889a241a285d062fd23860273ceecfc53a6e360a
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-03-04 17:20:31 +0000

    Onboard Secrets Manager: list subcommand

--- internal/cmd/secrets-manager/instance/list/list.go
+++ internal/cmd/secrets-manager/instance/list/list.go
@@ -11,11 +11,11 @@ import (
 	"github.com/stackitcloud/stackit-cli/internal/pkg/flags"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/globalflags"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/projectname"
-	"github.com/stackitcloud/stackit-cli/internal/pkg/services/mongodbflex/client"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/services/secrets-manager/client"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/tables"
+	"github.com/stackitcloud/stackit-sdk-go/services/secretsmanager"
 
 	"github.com/spf13/cobra"
-	"github.com/stackitcloud/stackit-sdk-go/services/mongodbflex"
 )
 
 const (
@@ -30,19 +30,19 @@ type inputModel struct {
 func NewCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "list",
-		Short: "Lists all MongoDB Flex instances",
-		Long:  "Lists all MongoDB Flex instances.",
+		Short: "Lists all Secrets Manager instances",
+		Long:  "Lists all Secrets Manager instances.",
 		Args:  args.NoArgs,
 		Example: examples.Build(
 			examples.NewExample(
-				`List all MongoDB Flex instances`,
-				"$ stackit mongodbflex instance list"),
+				`List all Secrets Manager instances`,
+				"$ stackit secrets-manager instance list"),
 			examples.NewExample(
-				`List all MongoDB Flex instances in JSON format`,
-				"$ stackit mongodbflex instance list --output-format json"),
+				`List all Secrets Manager instances in JSON format`,
+				"$ stackit secrets-manager instance list --output-format json"),
 			examples.NewExample(
-				`List up to 10 MongoDB Flex instances`,
-				"$ stackit mongodbflex instance list --limit 10"),
+				`List up to 10 Secrets Manager instances`,
+				"$ stackit secrets-manager instance list --limit 10"),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := context.Background()
@@ -61,9 +61,10 @@ func NewCmd() *cobra.Command {
 			req := buildRequest(ctx, model, apiClient)
 			resp, err := req.Execute()
 			if err != nil {
-				return fmt.Errorf("get MongoDB Flex instances: %w", err)
+				return fmt.Errorf("get Secret Manager instances: %w", err)
 			}
-			if resp.Items == nil || len(*resp.Items) == 0 {
+
+			if resp.Instances == nil || len(*resp.Instances) == 0 {
 				projectLabel, err := projectname.GetProjectName(ctx, cmd)
 				if err != nil {
 					projectLabel = model.ProjectId
@@ -71,7 +72,7 @@ func NewCmd() *cobra.Command {
 				cmd.Printf("No instances found for project %q\n", projectLabel)
 				return nil
 			}
-			instances := *resp.Items
+			instances := *resp.Instances
 
 			// Truncate output
 			if model.Limit != nil && len(instances) > int(*model.Limit) {
@@ -110,27 +111,28 @@ func parseInput(cmd *cobra.Command) (*inputModel, error) {
 	}, nil
 }
 
-func buildRequest(ctx context.Context, model *inputModel, apiClient *mongodbflex.APIClient) mongodbflex.ApiListInstancesRequest {
-	req := apiClient.ListInstances(ctx, model.ProjectId).Tag("")
+func buildRequest(ctx context.Context, model *inputModel, apiClient *secretsmanager.APIClient) secretsmanager.ApiListInstancesRequest {
+	req := apiClient.ListInstances(ctx, model.ProjectId)
 	return req
 }
 
-func outputResult(cmd *cobra.Command, outputFormat string, instances []mongodbflex.InstanceListInstance) error {
+func outputResult(cmd *cobra.Command, outputFormat string, instances []secretsmanager.Instance) error {
 	switch outputFormat {
 	case globalflags.JSONOutputFormat:
 		details, err := json.MarshalIndent(instances, "", "  ")
 		if err != nil {
-			return fmt.Errorf("marshal MongoDB Flex instance list: %w", err)
+			return fmt.Errorf("marshal Secret Manager instance list: %w", err)
 		}
 		cmd.Println(string(details))
 
 		return nil
 	default:
 		table := tables.NewTable()
-		table.SetHeader("ID", "NAME", "STATUS")
+		table.SetHeader("ID", "NAME", "STATE", "SECRETS")
+		// TODO: Add support for Number of Users (to match Portal)
 		for i := range instances {
 			instance := instances[i]
-			table.AddRow(*instance.Id, *instance.Name, *instance.Status)
+			table.AddRow(*instance.Id, *instance.Name, *instance.State, *instance.SecretCount)
 		}
 		err := table.Display(cmd)
 		if err != nil {
---
+++ internal/cmd/secrets-manager/secrets_manager.go
@@ -0,0 +1,25 @@
+package secretsmanager
+
+import (
+	"github.com/stackitcloud/stackit-cli/internal/cmd/secrets-manager/instance"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/args"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/utils"
+
+	"github.com/spf13/cobra"
+)
+
+func NewCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "secrets-manager",
+		Short: "Provides functionality for the Secrets Manager",
+		Long:  "Provides functionality for the Secrets Manager.",
+		Args:  args.NoArgs,
+		Run:   utils.CmdHelp,
+	}
+	addSubcommands(cmd)
+	return cmd
+}
+
+func addSubcommands(cmd *cobra.Command) {
+	cmd.AddCommand(instance.NewCmd())
+}
