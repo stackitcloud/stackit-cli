f0fc4242fe30845fe1da38ed15f165d7ad6f0d15
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-03-15 13:18:59 +0100

    Onboard Argus (instance): list and describe commands (#155)
    * Onboard Argus Instance: list command
    
    * Onboard Argus Instance: describe command
    
    * describe command: dont display name if empty
    
    * describe command: update output
    
    * describe command: remove additional check
    
    * describe command: add grafana url
    
    * add docs and address comments

--- internal/cmd/argus/instance/instance.go
+++ internal/cmd/argus/instance/instance.go
@@ -3,6 +3,8 @@ package instance
 import (
 	"github.com/stackitcloud/stackit-cli/internal/cmd/argus/instance/create"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/argus/instance/delete"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/argus/instance/describe"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/argus/instance/list"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/argus/instance/update"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/args"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/utils"
@@ -26,4 +28,6 @@ func addSubcommands(cmd *cobra.Command) {
 	cmd.AddCommand(create.NewCmd())
 	cmd.AddCommand(update.NewCmd())
 	cmd.AddCommand(delete.NewCmd())
+	cmd.AddCommand(list.NewCmd())
+	cmd.AddCommand(describe.NewCmd())
 }
