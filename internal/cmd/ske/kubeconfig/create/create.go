683cd8e635472be8d7232a3771a9a6cbc9d8624e
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-04-01 15:13:39 +0100

    finish create command implementation, add testing

--- internal/cmd/ske/kubeconfig/create/create.go
+++ internal/cmd/ske/kubeconfig/create/create.go
@@ -3,6 +3,8 @@ package create
 import (
 	"context"
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/stackitcloud/stackit-cli/internal/pkg/args"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/confirm"
@@ -11,6 +13,7 @@ import (
 	"github.com/stackitcloud/stackit-cli/internal/pkg/flags"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/globalflags"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/services/ske/client"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/utils"
 
 	"github.com/spf13/cobra"
 	"github.com/stackitcloud/stackit-sdk-go/services/ske"
@@ -20,11 +23,13 @@ const (
 	clusterNameArg = "CLUSTER_NAME"
 
 	expirationFlag = "expiration"
+	locationFlag   = "location"
 )
 
 type inputModel struct {
 	*globalflags.GlobalFlagModel
 	ClusterName    string
+	KubeconfigPath *string
 	ExpirationTime *string
 }
 
@@ -32,12 +37,23 @@ func NewCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   fmt.Sprintf("create %s", clusterNameArg),
 		Short: "Creates a kubeconfig for an SKE cluster",
-		Long:  "Creates a kubeconfig for a STACKIT Kubernetes Engine (SKE) cluster.",
-		Args:  args.SingleArg(clusterNameArg, nil),
+		Long: fmt.Sprintf("%s\n%s",
+			"Creates a kubeconfig for a STACKIT Kubernetes Engine (SKE) cluster.",
+			"By default the kubeconfig is created in the .kube folder, in the user's home directory. The kubeconfig file will be overwritten if it already exists."),
+		Args: args.SingleArg(clusterNameArg, nil),
 		Example: examples.Build(
 			examples.NewExample(
 				`Create a kubeconfig for the SKE cluster with name "my-cluster"`,
 				"$ stackit ske kubeconfig create my-cluster"),
+			examples.NewExample(
+				`Create a kubeconfig for the SKE cluster with name "my-cluster" and set the expiration time to 30 days`,
+				"$ stackit ske kubeconfig create my-cluster --expiration 30d"),
+			examples.NewExample(
+				`Create a kubeconfig for the SKE cluster with name "my-cluster" and set the expiration time to 2 months`,
+				"$ stackit ske kubeconfig create my-cluster --expiration 2M"),
+			examples.NewExample(
+				`Create a kubeconfig for the SKE cluster with name "my-cluster" in a custom location`,
+				"$ stackit ske kubeconfig create my-cluster --location /path/to/config"),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := context.Background()
@@ -53,7 +69,7 @@ func NewCmd() *cobra.Command {
 			}
 
 			if !model.AssumeYes {
-				prompt := fmt.Sprintf("Are you sure you want to start the rotation of the credentials for SKE cluster %q?", model.ClusterName)
+				prompt := fmt.Sprintf("Are you sure you want to create a kubeconfig for SKE cluster %q? This will OVERWRITE your current configuration, if it exists.", model.ClusterName)
 				err = confirm.PromptForConfirmation(cmd, prompt)
 				if err != nil {
 					return err
@@ -67,8 +83,28 @@ func NewCmd() *cobra.Command {
 				return fmt.Errorf("create kubeconfig for SKE cluster: %w", err)
 			}
 
-			// Output kubeconfig to stdout
-			fmt.Println(*resp.Kubeconfig)
+			// Create a config file in $HOME/.kube/config
+			configPath := model.KubeconfigPath
+
+			if configPath == nil {
+				userHome, err := os.UserHomeDir()
+				if err != nil {
+					return fmt.Errorf("could not get user home directory: %w", err)
+				}
+
+				err = os.MkdirAll(fmt.Sprintf("%s/.kube", userHome), 0700)
+				if err != nil {
+					return fmt.Errorf("could not create kube directory: %w", err)
+				}
+				configPath = utils.Ptr(fmt.Sprintf("%s/.kube", userHome))
+			}
+
+			err = os.WriteFile(fmt.Sprintf("%s/config", *configPath), []byte(*resp.Kubeconfig), 0600)
+			if err != nil {
+				return fmt.Errorf("could not write kubeconfig file: %w", err)
+			}
+
+			fmt.Printf("Created kubeconfig file for cluster %s with expiration date %v.\n", model.ClusterName, *resp.ExpirationTimestamp)
 
 			return nil
 		},
@@ -78,7 +114,8 @@ func NewCmd() *cobra.Command {
 }
 
 func configureFlags(cmd *cobra.Command) {
-	cmd.Flags().StringP(expirationFlag, "e", "", "Expiration time for the kubeconfig")
+	cmd.Flags().StringP(expirationFlag, "e", "", "Expiration time for the kubeconfig in seconds(s), minutes(m), hours(h), days(d) or months(M). Example: 30d. By default, expiration time is 1h")
+	cmd.Flags().String(locationFlag, "", "Folder location to store the kubeconfig file. By default, the kubeconfig is created in the .kube folder, in the user's home directory.")
 }
 
 func parseInput(cmd *cobra.Command, inputArgs []string) (*inputModel, error) {
@@ -89,11 +126,63 @@ func parseInput(cmd *cobra.Command, inputArgs []string) (*inputModel, error) {
 		return nil, &errors.ProjectIdError{}
 	}
 
+	expirationTimeInput := flags.FlagToStringPointer(cmd, expirationFlag)
+	var expirationTime *string
+	if expirationTimeInput != nil {
+		expirationTime = convertToSeconds(*expirationTimeInput)
+		if expirationTime == nil {
+			return nil, fmt.Errorf("invalid expiration time: %s", *expirationTimeInput)
+		}
+	}
+
 	return &inputModel{
 		GlobalFlagModel: globalFlags,
 		ClusterName:     clusterName,
-		ExpirationTime:  flags.FlagToStringPointer(cmd, expirationFlag),
+		KubeconfigPath:  flags.FlagToStringPointer(cmd, locationFlag),
+		ExpirationTime:  expirationTime,
 	}, nil
+}
+
+func convertToSeconds(timeStr string) *string {
+	if len(timeStr) < 2 {
+		return nil
+	}
+
+	unit := timeStr[len(timeStr)-1:]
+	if _, err := strconv.Atoi(unit); err == nil {
+		// If the last character is a digit, assume the whole string is a number of seconds
+		return utils.Ptr(timeStr)
+	}
+
+	valueStr := timeStr[:len(timeStr)-1]
+	value, err := strconv.ParseUint(valueStr, 10, 64)
+	if err != nil {
+		return nil
+	}
+
+	var multiplier uint64
+	switch unit {
+	// second
+	case "s":
+		multiplier = 1
+	// minute
+	case "m":
+		multiplier = 60
+	// hour
+	case "h":
+		multiplier = 60 * 60
+	// day
+	case "d":
+		multiplier = 60 * 60 * 24
+	// month, assume 30 days
+	case "M":
+		multiplier = 60 * 60 * 24 * 30
+	default:
+		return nil
+	}
+
+	result := uint64(value) * multiplier
+	return utils.Ptr(strconv.FormatUint(result, 10))
 }
 
 func buildRequest(ctx context.Context, model *inputModel, apiClient *ske.APIClient) ske.ApiCreateKubeconfigRequest {
--- internal/cmd/ske/kubeconfig/create/create_test.go
+++ internal/cmd/ske/kubeconfig/create/create_test.go
@@ -1,1 +1,298 @@
 package create
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stackitcloud/stackit-cli/internal/pkg/globalflags"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/utils"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/google/uuid"
+	"github.com/stackitcloud/stackit-sdk-go/services/ske"
+)
+
+var projectIdFlag = globalflags.ProjectIdFlag
+
+type testCtxKey struct{}
+
+var testCtx = context.WithValue(context.Background(), testCtxKey{}, "foo")
+var testClient = &ske.APIClient{}
+var testProjectId = uuid.NewString()
+var testClusterName = "cluster"
+
+func fixtureArgValues(mods ...func(argValues []string)) []string {
+	argValues := []string{
+		testClusterName,
+	}
+	for _, mod := range mods {
+		mod(argValues)
+	}
+	return argValues
+}
+
+func fixtureFlagValues(mods ...func(flagValues map[string]string)) map[string]string {
+	flagValues := map[string]string{
+		projectIdFlag: testProjectId,
+	}
+	for _, mod := range mods {
+		mod(flagValues)
+	}
+	return flagValues
+}
+
+func fixtureInputModel(mods ...func(model *inputModel)) *inputModel {
+	model := &inputModel{
+		GlobalFlagModel: &globalflags.GlobalFlagModel{
+			ProjectId: testProjectId,
+		},
+		ClusterName: testClusterName,
+	}
+	for _, mod := range mods {
+		mod(model)
+	}
+	return model
+}
+
+func fixtureRequest(mods ...func(request *ske.ApiCreateKubeconfigRequest)) ske.ApiCreateKubeconfigRequest {
+	request := testClient.CreateKubeconfig(testCtx, testProjectId, testClusterName)
+	request = request.CreateKubeconfigPayload(ske.CreateKubeconfigPayload{})
+	for _, mod := range mods {
+		mod(&request)
+	}
+	return request
+}
+
+func TestParseInput(t *testing.T) {
+	tests := []struct {
+		description   string
+		argValues     []string
+		flagValues    map[string]string
+		isValid       bool
+		expectedModel *inputModel
+	}{
+		{
+			description:   "base",
+			argValues:     fixtureArgValues(),
+			flagValues:    fixtureFlagValues(),
+			isValid:       true,
+			expectedModel: fixtureInputModel(),
+		},
+		{
+			description: "1000s expiration time",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				flagValues["expiration"] = "1000s"
+			}),
+			isValid: true,
+			expectedModel: fixtureInputModel(func(model *inputModel) {
+				model.ExpirationTime = utils.Ptr("1000")
+			}),
+		},
+		{
+			description: "1000 expiration time",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				flagValues["expiration"] = "1000"
+			}),
+			isValid: true,
+			expectedModel: fixtureInputModel(func(model *inputModel) {
+				model.ExpirationTime = utils.Ptr("1000")
+			}),
+		},
+		{
+			description: "60m expiration time",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				flagValues["expiration"] = "60m"
+			}),
+			isValid: true,
+			expectedModel: fixtureInputModel(func(model *inputModel) {
+				model.ExpirationTime = utils.Ptr("3600")
+			}),
+		},
+		{
+			description: "4h expiration time",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				flagValues["expiration"] = "4h"
+			}),
+			isValid: true,
+			expectedModel: fixtureInputModel(func(model *inputModel) {
+				model.ExpirationTime = utils.Ptr("14400")
+			}),
+		},
+		{
+			description: "2d expiration time",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				flagValues["expiration"] = "2d"
+			}),
+			isValid: true,
+			expectedModel: fixtureInputModel(func(model *inputModel) {
+				model.ExpirationTime = utils.Ptr("172800")
+			}),
+		},
+		{
+			description: "2M expiration time",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				flagValues["expiration"] = "2M"
+			}),
+			isValid: true,
+			expectedModel: fixtureInputModel(func(model *inputModel) {
+				model.ExpirationTime = utils.Ptr("5184000")
+			}),
+		},
+		{
+			description: "invalid expiration time",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				flagValues["expiration"] = "2A"
+			}),
+			isValid: false,
+		},
+		{
+			description: "custom location",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				flagValues["location"] = "/path/to/config"
+			}),
+			isValid: true,
+			expectedModel: fixtureInputModel(func(model *inputModel) {
+				model.KubeconfigPath = utils.Ptr("/path/to/config")
+			}),
+		},
+		{
+			description: "no values",
+			argValues:   []string{},
+			flagValues:  map[string]string{},
+			isValid:     false,
+		},
+		{
+			description: "no arg values",
+			argValues:   []string{},
+			flagValues:  fixtureFlagValues(),
+			isValid:     false,
+		},
+		{
+			description: "no flag values",
+			argValues:   fixtureArgValues(),
+			flagValues:  map[string]string{},
+			isValid:     false,
+		},
+		{
+			description: "project id missing",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				delete(flagValues, projectIdFlag)
+			}),
+			isValid: false,
+		},
+		{
+			description: "project id invalid 1",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				flagValues[projectIdFlag] = ""
+			}),
+			isValid: false,
+		},
+		{
+			description: "project id invalid 2",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				flagValues[projectIdFlag] = "invalid-uuid"
+			}),
+			isValid: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			cmd := NewCmd()
+			err := globalflags.Configure(cmd.Flags())
+			if err != nil {
+				t.Fatalf("configure global flags: %v", err)
+			}
+
+			for flag, value := range tt.flagValues {
+				err := cmd.Flags().Set(flag, value)
+				if err != nil {
+					if !tt.isValid {
+						return
+					}
+					t.Fatalf("setting flag --%s=%s: %v", flag, value, err)
+				}
+			}
+
+			err = cmd.ValidateArgs(tt.argValues)
+			if err != nil {
+				if !tt.isValid {
+					return
+				}
+				t.Fatalf("error validating args: %v", err)
+			}
+
+			err = cmd.ValidateRequiredFlags()
+			if err != nil {
+				if !tt.isValid {
+					return
+				}
+				t.Fatalf("error validating flags: %v", err)
+			}
+
+			model, err := parseInput(cmd, tt.argValues)
+			if err != nil {
+				if !tt.isValid {
+					return
+				}
+				t.Fatalf("error parsing flags: %v", err)
+			}
+
+			if !tt.isValid {
+				t.Fatalf("did not fail on invalid input")
+			}
+			diff := cmp.Diff(model, tt.expectedModel)
+			if diff != "" {
+				t.Fatalf("Data does not match: %s", diff)
+			}
+		})
+	}
+}
+
+func TestBuildRequest(t *testing.T) {
+	tests := []struct {
+		description     string
+		model           *inputModel
+		expectedRequest ske.ApiCreateKubeconfigRequest
+	}{
+		{
+			description:     "base",
+			model:           fixtureInputModel(),
+			expectedRequest: fixtureRequest(),
+		},
+		{
+			description: "expiration time",
+			model: fixtureInputModel(func(model *inputModel) {
+				model.ExpirationTime = utils.Ptr("30000")
+			}),
+			expectedRequest: fixtureRequest().CreateKubeconfigPayload(ske.CreateKubeconfigPayload{
+				ExpirationSeconds: utils.Ptr("30000")}),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			request := buildRequest(testCtx, tt.model, testClient)
+
+			diff := cmp.Diff(request, tt.expectedRequest,
+				cmp.AllowUnexported(tt.expectedRequest),
+				cmpopts.EquateComparable(testCtx),
+			)
+			if diff != "" {
+				t.Fatalf("Data does not match: %s", diff)
+			}
+		})
+	}
+}
