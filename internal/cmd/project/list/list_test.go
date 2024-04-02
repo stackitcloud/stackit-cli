f74223819ea2bb85a0ca647700b9cc0c18004152
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-04-02 11:57:04 +0100

    fix linting

--- internal/cmd/project/list/list_test.go
+++ internal/cmd/project/list/list_test.go
@@ -256,7 +256,6 @@ func TestParseInput(t *testing.T) {
 }
 
 func TestBuildRequest(t *testing.T) {
-
 	authUserEmail, err := auth.GetAuthField(auth.USER_EMAIL)
 	if err != nil {
 		t.Fatalf("Failed to get auth user email: %v", err)
