340a556c4fda23e4e601fbf9508af877db4fdb33
João Palet <joao.palet@freiheit.com>
2024-04-02 10:42:13 +0100

    Only write to config file when needed

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -87,11 +87,6 @@ func InitConfig() {
 	cobra.CheckErr(err)
 	setConfigDefaults()
 
-	err = viper.WriteConfigAs(configFilePath)
-	cobra.CheckErr(err)
-
-	// Needs to be done after WriteConfigAs, otherwise it would write
-	// the environment variables to the config file
 	viper.AutomaticEnv()
 	viper.SetEnvPrefix("stackit")
 }
