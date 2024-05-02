9cf74c9e7fcb7352f47a47510472d1a74921b2ae
Gökçe Gök Klingel <goekce.goek_klingel@mail.schwarz>
2024-05-02 11:53:33 +0200

    add debug logs to the create command

--- internal/cmd/load-balancer/create/create.go
+++ internal/cmd/load-balancer/create/create.go
@@ -139,10 +139,21 @@ func parseInput(p *print.Printer, cmd *cobra.Command) (*inputModel, error) {
 		}
 	}
 
-	return &inputModel{
+	model := inputModel{
 		GlobalFlagModel: globalFlags,
 		Payload:         payload,
-	}, nil
+	}
+
+	if p.IsVerbosityDebug() {
+		modelStr, err := print.BuildDebugStrFromInputModel(model)
+		if err != nil {
+			p.Debug(print.ErrorLevel, "convert model to string for debugging: %v", err)
+		} else {
+			p.Debug(print.DebugLevel, "parsed input values: %s", modelStr)
+		}
+	}
+
+	return &model, nil
 }
 
 func buildRequest(ctx context.Context, model *inputModel, apiClient *loadbalancer.APIClient) loadbalancer.ApiCreateLoadBalancerRequest {
