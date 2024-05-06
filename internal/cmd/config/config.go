174b46879971c5b4769f9b837275ed775ac8bf38
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-05-06 14:56:29 +0100

    Update internal/cmd/config/config.go
    Co-authored-by: Vicente Pinto <vicente.pinto@freiheit.com>

--- internal/cmd/config/config.go
+++ internal/cmd/config/config.go
@@ -17,7 +17,7 @@ func NewCmd(p *print.Printer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "config",
 		Short: "Provides functionality for CLI configuration options",
-		Long:  fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n%s", "Provides functionality for CLI configuration options", "The configuration is stored in a file in the user's config directory, which is OS dependent.", "Windows: %APPDATA%\\stackit", "Linux: $XDG_CONFIG_HOME/stackit or $HOME/.config/stackit", "macOS: $HOME/Library/Application Support/stackit", "The configuration file is named `cli-config.json` and is created automatically when setting a configuration option."),
+		Long:  fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n%s", "Provides functionality for CLI configuration options", "The configuration is stored in a file in the user's config directory, which is OS dependent.", "Windows: %APPDATA%\\stackit", "Linux: $XDG_CONFIG_HOME/stackit or $HOME/.config/stackit", "macOS: $HOME/Library/Application Support/stackit", "The configuration file is named `cli-config.json` and is created automatically in your first CLI run."),
 		Args:  args.NoArgs,
 		Run:   utils.CmdHelp,
 	}
