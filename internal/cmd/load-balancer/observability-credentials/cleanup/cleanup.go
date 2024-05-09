1ce0c43286c3504149a43a628925d09b230b227f
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-05-09 17:31:14 +0100

    command implementation, add testing

---
+++ internal/cmd/load-balancer/observability-credentials/cleanup/cleanup.go
@@ -0,0 +1,141 @@
+package cleanup
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/stackitcloud/stackit-cli/internal/pkg/args"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/errors"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/examples"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/globalflags"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/print"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/projectname"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/services/load-balancer/client"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/services/load-balancer/utils"
+
+	"github.com/spf13/cobra"
+	"github.com/stackitcloud/stackit-sdk-go/services/loadbalancer"
+)
+
+type inputModel struct {
+	*globalflags.GlobalFlagModel
+}
+
+func NewCmd(p *print.Printer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   fmt.Sprintf("cleanup"),
+		Short: "Deletes observability credentials unused by any Load Balancer",
+		Long:  "Deletes observability credentials unused by any Load Balancer.",
+		Args:  args.NoArgs,
+		Example: examples.Build(
+			examples.NewExample(
+				`Delete observability credentials unused by any Load Balancer`,
+				"$ stackit load-balancer observability-credentials cleanup"),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			model, err := parseInput(p, cmd)
+			if err != nil {
+				return err
+			}
+
+			// Configure API client
+			apiClient, err := client.ConfigureClient(p)
+			if err != nil {
+				return err
+			}
+
+			projectLabel, err := projectname.GetProjectName(ctx, p, cmd)
+			if err != nil {
+				p.Debug(print.ErrorLevel, "get project name: %v", err)
+				projectLabel = model.ProjectId
+			}
+
+			listReq := buildListCredentialsRequest(ctx, model, apiClient)
+			resp, err := listReq.Execute()
+			if err != nil {
+				return fmt.Errorf("list Load Balancer observability credentials: %w", err)
+			}
+			credentialsPtr := resp.Credentials
+
+			var credentials []loadbalancer.CredentialsResponse
+			if credentialsPtr != nil && len(*credentialsPtr) > 0 {
+				credentials, err = utils.FilterCredentials(ctx, apiClient, *credentialsPtr, model.ProjectId, utils.OP_FILTER_UNUSED)
+				if err != nil {
+					return fmt.Errorf("filter Load Balancer observability credentials: %w", err)
+				}
+			}
+
+			if len(credentials) == 0 {
+				p.Info("No unused observability credentials found on project %q\n", projectLabel)
+				return nil
+			}
+
+			if !model.AssumeYes {
+				prompt := "Will delete the following unused observability credentials: \n"
+				for _, credential := range credentials {
+					if credential.DisplayName == nil || credential.Username == nil {
+						return fmt.Errorf("delete Load Balancer observability credentials: missing display name or username")
+					}
+					name := *credential.DisplayName
+					username := *credential.Username
+					prompt += fmt.Sprintf("  - %s (username: %q)\n", name, username)
+				}
+				prompt += fmt.Sprintf("Are you sure you want to delete unused observability credentials on project %q? (This cannot be undone)", projectLabel)
+				err = p.PromptForConfirmation(prompt)
+				if err != nil {
+					return err
+				}
+			}
+
+			for _, credential := range credentials {
+				if credential.CredentialsRef == nil {
+					return fmt.Errorf("delete Load Balancer observability credentials: missing credentials reference")
+				}
+				credentialsRef := *credential.CredentialsRef
+				// Call API
+				req := buildDeleteCredentialRequest(ctx, model, apiClient, credentialsRef)
+				_, err = req.Execute()
+				if err != nil {
+					return fmt.Errorf("delete Load Balancer observability credentials: %w", err)
+				}
+			}
+
+			p.Info("Deleted unused observability credentials on project %q\n", projectLabel)
+			return nil
+		},
+	}
+	return cmd
+}
+
+func parseInput(p *print.Printer, cmd *cobra.Command) (*inputModel, error) {
+	globalFlags := globalflags.Parse(p, cmd)
+	if globalFlags.ProjectId == "" {
+		return nil, &errors.ProjectIdError{}
+	}
+
+	model := inputModel{
+		GlobalFlagModel: globalFlags,
+	}
+
+	if p.IsVerbosityDebug() {
+		modelStr, err := print.BuildDebugStrFromInputModel(model)
+		if err != nil {
+			p.Debug(print.ErrorLevel, "convert model to string for debugging: %v", err)
+		} else {
+			p.Debug(print.DebugLevel, "parsed input values: %s", modelStr)
+		}
+	}
+
+	return &model, nil
+}
+
+func buildDeleteCredentialRequest(ctx context.Context, model *inputModel, apiClient *loadbalancer.APIClient, credentialsRef string) loadbalancer.ApiDeleteCredentialsRequest {
+	req := apiClient.DeleteCredentials(ctx, model.ProjectId, credentialsRef)
+	return req
+}
+
+func buildListCredentialsRequest(ctx context.Context, model *inputModel, apiClient *loadbalancer.APIClient) loadbalancer.ApiListCredentialsRequest {
+	req := apiClient.ListCredentials(ctx, model.ProjectId)
+	return req
+}
