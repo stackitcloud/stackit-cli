b810e2b1b32721a146f928aba67d9ad31e002f62
Gökçe Gök Klingel <goekce.goek_klingel@mail.schwarz>
2024-05-14 14:03:00 +0200

    tidy up imports

--- internal/cmd/opensearch/instance/create/create.go
+++ internal/cmd/opensearch/instance/create/create.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-
 	"strings"
 
 	"github.com/ghodss/yaml"
--- internal/cmd/secrets-manager/instance/describe/describe.go
+++ internal/cmd/secrets-manager/instance/describe/describe.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-
 	"strings"
 
 	"github.com/ghodss/yaml"
