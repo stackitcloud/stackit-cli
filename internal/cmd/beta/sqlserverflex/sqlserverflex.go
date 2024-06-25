1cc43888135d9985d71c9ce9affcac457248d177
João Palet <vicente.pinto@freiheit.com>
2024-06-25 11:22:30 +0100

    Test

--- internal/cmd/beta/sqlserverflex/sqlserverflex.go
+++ internal/cmd/beta/sqlserverflex/sqlserverflex.go
@@ -14,7 +14,7 @@ import (
 
 func NewCmd(p *print.Printer) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "sqlserverflex",
+		Use:   "sqlserverflex TEST",
 		Short: "Provides functionality for SQLServer Flex",
 		Long:  "Provides functionality for SQLServer Flex.",
 		Args:  args.NoArgs,
