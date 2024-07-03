4409338f9cdc5c350a1b2626aa8400ddc92d0db5
test.test@freiheit.com <joao.palet@freiheit.com>
2024-07-03 15:59:34 +0100

    Add warning regarding migration to new IDP

--- internal/cmd/auth/login/login.go
+++ internal/cmd/auth/login/login.go
@@ -23,6 +23,14 @@ func NewCmd(p *print.Printer) *cobra.Command {
 				"$ stackit auth login"),
 		),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			p.Warn(fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n\n",
+				"Starting on July 9 2024, the new STACKIT Identity Provider (IDP) will be available.",
+				"On this date, we will release a new version of the STACKIT CLI that will use the new IDP for user authentication.",
+				"This also means that the user authentication on STACKIT CLI versions released before July 9 2024 is no longer guaranteed to work for all services.",
+				"Please make sure to update your STACKIT CLI to the latest version after July 9 2024 to ensure that you can continue to use all STACKIT services.",
+				"You can find more information regarding the new IDP at https://docs.stackit.cloud/stackit/en/release-notes-23101442.html#ReleaseNotes-2024-06-21-identity-provider.",
+			))
+
 			err := auth.AuthorizeUser(p, false)
 			if err != nil {
 				return fmt.Errorf("authorization failed: %w", err)
