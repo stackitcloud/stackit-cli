20a544eace8fa33d3af8fa033c74fa3fb729bbac
test.test@freiheit.com <joao.palet@freiheit.com>
2024-07-04 13:32:09 +0100

    Remove IDO custom flags from command, will live only in config commands

--- internal/cmd/auth/login/login.go
+++ internal/cmd/auth/login/login.go
@@ -5,24 +5,11 @@ import (
 
 	"github.com/stackitcloud/stackit-cli/internal/pkg/args"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/auth"
-	"github.com/stackitcloud/stackit-cli/internal/pkg/config"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/examples"
-	"github.com/stackitcloud/stackit-cli/internal/pkg/flags"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/print"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
-
-const (
-	identityProviderCustomEndpointFlag = "identity-provider-custom-endpoint"
-	identityProviderCustomClientIdFlag = "identity-provider-custom-client-id"
-)
-
-type inputModel struct {
-	IdentityProviderCustomEndpoint string
-	IdentityProviderCustomClientId string
-}
 
 func NewCmd(p *print.Printer) *cobra.Command {
 	cmd := &cobra.Command{
@@ -44,18 +31,6 @@ func NewCmd(p *print.Printer) *cobra.Command {
 				"You can find more information regarding the new IDP at https://docs.stackit.cloud/stackit/en/release-notes-23101442.html#ReleaseNotes-2024-06-21-identity-provider",
 			))
 
-			model := parseInput(p, cmd)
-
-			// Set value in config but do not persist it
-			if model.IdentityProviderCustomEndpoint != "" {
-				viper.Set(config.IdentityProviderCustomEndpointKey, model.IdentityProviderCustomEndpoint)
-			}
-
-			// Set value in config but do not persist it
-			if model.IdentityProviderCustomClientId != "" {
-				viper.Set(config.IdentityProviderCustomClientIdKey, model.IdentityProviderCustomClientId)
-			}
-
 			err := auth.AuthorizeUser(p, false)
 			if err != nil {
 				return fmt.Errorf("authorization failed: %w", err)
@@ -65,29 +40,5 @@ func NewCmd(p *print.Printer) *cobra.Command {
 			return nil
 		},
 	}
-	configureFlags(cmd)
 	return cmd
-}
-
-func configureFlags(cmd *cobra.Command) {
-	cmd.Flags().String(identityProviderCustomEndpointFlag, "", "Identity Provider base URL")
-	cmd.Flags().String(identityProviderCustomClientIdFlag, "", "Identity Provider client ID")
-}
-
-func parseInput(p *print.Printer, cmd *cobra.Command) *inputModel {
-	model := inputModel{
-		IdentityProviderCustomEndpoint: flags.FlagToStringValue(p, cmd, identityProviderCustomEndpointFlag),
-		IdentityProviderCustomClientId: flags.FlagToStringValue(p, cmd, identityProviderCustomClientIdFlag),
-	}
-
-	if p.IsVerbosityDebug() {
-		modelStr, err := print.BuildDebugStrFromInputModel(model)
-		if err != nil {
-			p.Debug(print.ErrorLevel, "convert model to string for debugging: %v", err)
-		} else {
-			p.Debug(print.DebugLevel, "parsed input values: %s", modelStr)
-		}
-	}
-
-	return &model
 }
