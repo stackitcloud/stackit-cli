6dfc5f355b0f142d81cee0b7d64c99ff3718d5e3
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-07-24 12:03:54 +0100

    remove delete command

--- internal/cmd/beta/network-area/network_area.go
+++ internal/cmd/beta/network-area/network_area.go
@@ -2,7 +2,6 @@ package networkarea
 
 import (
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/network-area/create"
-	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/network-area/delete"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/network-area/describe"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/network-area/list"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/network-area/update"
@@ -27,7 +26,6 @@ func NewCmd(p *print.Printer) *cobra.Command {
 
 func addSubcommands(cmd *cobra.Command, p *print.Printer) {
 	cmd.AddCommand(create.NewCmd(p))
-	cmd.AddCommand(delete.NewCmd(p))
 	cmd.AddCommand(describe.NewCmd(p))
 	cmd.AddCommand(list.NewCmd(p))
 	cmd.AddCommand(update.NewCmd(p))
