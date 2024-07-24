fe11b42ab2d48dee58a0c34fea6997847154a8df
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-07-24 11:58:07 +0100

    list and describe

--- internal/cmd/beta/network-area/network_area.go
+++ internal/cmd/beta/network-area/network_area.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/network-area/list"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/network-area/delete"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/network-area/describe"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/network-area/list"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/network-area/update"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/args"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/print"
@@ -33,4 +34,6 @@ func addSubcommands(cmd *cobra.Command, p *print.Printer) {
 	cmd.AddCommand(update.NewCmd(p))
 	cmd.AddCommand(delete.NewCmd(p))
 	cmd.AddCommand(describe.NewCmd(p))
+	cmd.AddCommand(list.NewCmd(p))
+	cmd.AddCommand(update.NewCmd(p))
 }
