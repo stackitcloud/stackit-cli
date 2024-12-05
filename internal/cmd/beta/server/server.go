0da6d09db4ec5e5b1ff06f32b7b916081bd8e0d5
Marcel <72880145+marceljk@users.noreply.github.com>
2024-12-05 16:46:00 +0100

    Onboard IaaS Server Service Account Commands (#522)
    * Add server service-account commands
    * Generate docs for server service-account commands

--- internal/cmd/beta/server/server.go
+++ internal/cmd/beta/server/server.go
@@ -8,8 +8,10 @@ import (
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/server/describe"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/server/list"
 	publicip "github.com/stackitcloud/stackit-cli/internal/cmd/beta/server/public-ip"
+	serviceaccount "github.com/stackitcloud/stackit-cli/internal/cmd/beta/server/service-account"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/server/update"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/server/volume"
+
 	"github.com/stackitcloud/stackit-cli/internal/pkg/args"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/print"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/utils"
@@ -37,6 +39,7 @@ func addSubcommands(cmd *cobra.Command, p *print.Printer) {
 	cmd.AddCommand(describe.NewCmd(p))
 	cmd.AddCommand(list.NewCmd(p))
 	cmd.AddCommand(publicip.NewCmd(p))
+	cmd.AddCommand(serviceaccount.NewCmd(p))
 	cmd.AddCommand(update.NewCmd(p))
 	cmd.AddCommand(volume.NewCmd(p))
 }
