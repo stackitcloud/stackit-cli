f77a9d631f77439ff0447dbb7a36243cb5fbce33
Rüdiger Schmitz <ruediger.schmitz@inovex.de>
2024-12-12 17:59:03 +0100

    restructed security-group command

--- internal/cmd/beta/security-group/security_group.go
+++ internal/cmd/beta/security-group/security_group.go
@@ -1,11 +1,8 @@
 package security_group
 
 import (
-	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/security-group/create"
-	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/security-group/delete"
-	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/security-group/describe"
-	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/security-group/list"
-	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/security-group/update"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/security-group/group"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/security-group/rule"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/args"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/print"
 
@@ -18,7 +15,7 @@ func NewCmd(p *print.Printer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "security-group",
 		Short: "Manage security groups",
-		Long:  "Manage the lifecycle of security groups.",
+		Long:  "Manage the lifecycle of security groups and rules.",
 		Args:  args.NoArgs,
 		Run:   utils.CmdHelp,
 	}
@@ -28,10 +25,7 @@ func NewCmd(p *print.Printer) *cobra.Command {
 
 func addSubcommands(cmd *cobra.Command, p *print.Printer) {
 	cmd.AddCommand(
-		create.NewCmd(p),
-		delete.NewCmd(p),
-		describe.NewCmd(p),
-		list.NewCmd(p),
-		update.NewCmd(p),
+		rule.NewCmd(p),
+		group.NewCmd(p),
 	)
 }
