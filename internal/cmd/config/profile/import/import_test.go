5787df14dd201f450603879c166b07c2665539cd
Marcel Jacek <Marcel.Jacek@stackit.cloud>
2024-12-16 11:54:42 +0100

    Add command to import config profiles

---
+++ internal/cmd/config/profile/import/import_test.go
@@ -0,0 +1,118 @@
+package _import
+
+import (
+	_ "embed"
+	"strconv"
+	"testing"
+
+	"github.com/stackitcloud/stackit-cli/internal/pkg/globalflags"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/print"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+const testProfile = "test-profile"
+const testConfig = "@./template/profile.json"
+const testNoSet = false
+
+//go:embed template/profile.json
+var testConfigContent string
+
+func fixtureFlagValues(mods ...func(flagValues map[string]string)) map[string]string {
+	flagValues := map[string]string{
+		nameFlag:   testProfile,
+		configFlag: testConfig,
+		noSetFlag:  strconv.FormatBool(testNoSet),
+	}
+	for _, mod := range mods {
+		mod(flagValues)
+	}
+	return flagValues
+}
+
+func fixtureInputModel(mods ...func(model *inputModel)) *inputModel {
+	model := &inputModel{
+		GlobalFlagModel: &globalflags.GlobalFlagModel{
+			Verbosity: globalflags.VerbosityDefault,
+		},
+		ProfileName: testProfile,
+		Config:      testConfigContent,
+		NoSet:       testNoSet,
+	}
+	for _, mod := range mods {
+		mod(model)
+	}
+	return model
+}
+
+func TestParseInput(t *testing.T) {
+	tests := []struct {
+		description   string
+		flagValues    map[string]string
+		isValid       bool
+		expectedModel *inputModel
+	}{
+		{
+			description:   "base",
+			flagValues:    fixtureFlagValues(),
+			isValid:       true,
+			expectedModel: fixtureInputModel(),
+		},
+		{
+			description: "no flags",
+			flagValues:  map[string]string{},
+			isValid:     false,
+		},
+		{
+			description: "invalid path",
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				flagValues[configFlag] = "@./template/invalid-file"
+			}),
+			isValid: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			p := print.NewPrinter()
+			cmd := NewCmd(p)
+			err := globalflags.Configure(cmd.Flags())
+			if err != nil {
+				t.Fatalf("configure global flags: %v", err)
+			}
+
+			for flag, value := range tt.flagValues {
+				err = cmd.Flags().Set(flag, value)
+				if err != nil {
+					if !tt.isValid {
+						return
+					}
+					t.Fatalf("setting flag --%s=%s: %v", flag, value, err)
+				}
+			}
+
+			err = cmd.ValidateRequiredFlags()
+			if err != nil {
+				if !tt.isValid {
+					return
+				}
+				t.Fatalf("error validating flags: %v", err)
+			}
+
+			model, err := parseInput(p, cmd)
+			if err != nil {
+				if !tt.isValid {
+					t.Fatalf("error parsing input: %v", err)
+				}
+			}
+
+			if !tt.isValid {
+				t.Fatalf("did not fail on invalid input")
+			}
+			diff := cmp.Diff(tt.expectedModel, model)
+			if diff != "" {
+				t.Fatalf("Data does not match: %s", diff)
+			}
+		})
+	}
+}
