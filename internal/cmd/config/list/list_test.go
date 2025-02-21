ddfb512cbdbd630ed66f520198fa8054097e9b33
Alexander Dahmen <alexander.dahmen@inovex.de>
2025-02-21 09:54:58 +0100

    chore(config): Add nil pointer checks and tests for the outputResult functions (#621)
    Signed-off-by: Alexander Dahmen <alexander.dahmen@inovex.de>

---
+++ internal/cmd/config/list/list_test.go
@@ -0,0 +1,35 @@
+package list
+
+import (
+	"testing"
+
+	"github.com/stackitcloud/stackit-cli/internal/pkg/print"
+)
+
+func TestOutputResult(t *testing.T) {
+	type args struct {
+		outputFormat  string
+		configData    map[string]any
+		activeProfile string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "empty",
+			args:    args{},
+			wantErr: false,
+		},
+	}
+	p := print.NewPrinter()
+	p.Cmd = NewCmd(p)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := outputResult(p, tt.args.outputFormat, tt.args.configData, tt.args.activeProfile); (err != nil) != tt.wantErr {
+				t.Errorf("outputResult() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
