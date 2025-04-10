95d97d9db707e72dec2afb4dc797551a6fe4ac7c
Rüdiger Schmitz <152157960+bahkauv70@users.noreply.github.com>
2025-04-10 10:32:20 +0200

    feat(credentials): code cleanup

--- internal/pkg/services/alb/utils/utils_test.go
+++ internal/pkg/services/alb/utils/utils_test.go
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 package utils
 
 type AlbClientMocked struct {
 }
-||||||| parent of db5dfa32 (feat(alb): list command)
-=======
-package utils
-
-type AlbClientMocked struct {
-}
-
->>>>>>> db5dfa32 (feat(alb): list command)
