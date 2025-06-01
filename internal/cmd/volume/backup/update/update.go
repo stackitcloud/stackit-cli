43b6bd5bda7c29773269edfb00948d38fde58f00
Benjosh95 <benjamin.fuertsch@inovex.de>
2025-06-01 19:54:42 +0200

    add volume backup update and update tests

---
+++ internal/cmd/volume/backup/update/update.go
@@ -0,0 +1,170 @@
+package update
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/goccy/go-yaml"
+	"github.com/spf13/cobra"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/params"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/args"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/errors"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/examples"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/flags"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/globalflags"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/print"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/services/iaas/client"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/utils"
+
+	"github.com/stackitcloud/stackit-sdk-go/services/iaas"
+)
+
+const (
+	backupIdArg = "BACKUP_ID"
+	nameFlag    = "name"
+	labelsFlag  = "labels"
+)
+
+type inputModel struct {
+	*globalflags.GlobalFlagModel
+	BackupId string
+	Name     *string
+	Labels   map[string]string
+}
+
+func NewCmd(params *params.CmdParams) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   fmt.Sprintf("update %s", backupIdArg),
+		Short: "Updates a backup",
+		Long:  "Updates a backup by its ID.",
+		Args:  args.SingleArg(backupIdArg, utils.ValidateUUID),
+		Example: examples.Build(
+			examples.NewExample(
+				`Update a backup name`,
+				"$ stackit volume backup update xxx-xxx-xxx --name new-name"),
+			examples.NewExample(
+				`Update backup labels`,
+				"$ stackit volume backup update xxx-xxx-xxx --labels key1=value1,key2=value2"),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			model, err := parseInput(params.Printer, cmd, args)
+			if err != nil {
+				return err
+			}
+
+			// Configure API client
+			apiClient, err := client.ConfigureClient(params.Printer, params.CliVersion)
+			if err != nil {
+				return err
+			}
+
+			// Call API
+			req := buildRequest(ctx, model, apiClient)
+			resp, err := req.Execute()
+			if err != nil {
+				return fmt.Errorf("update backup: %w", err)
+			}
+
+			// Get backup label (use ID if name not available)
+			backupLabel := model.BackupId
+			if resp.Name != nil {
+				backupLabel = *resp.Name
+			}
+
+			return outputResult(params.Printer, model.OutputFormat, backupLabel, resp)
+		},
+	}
+
+	configureFlags(cmd)
+	return cmd
+}
+
+func configureFlags(cmd *cobra.Command) {
+	cmd.Flags().String(nameFlag, "", "Name of the backup")
+	cmd.Flags().StringToString(labelsFlag, nil, "Key-value string pairs as labels")
+}
+
+func parseInput(p *print.Printer, cmd *cobra.Command, inputArgs []string) (*inputModel, error) {
+	backupId := inputArgs[0]
+
+	globalFlags := globalflags.Parse(p, cmd)
+	if globalFlags.ProjectId == "" {
+		return nil, &errors.ProjectIdError{}
+	}
+
+	name := flags.FlagToStringPointer(p, cmd, nameFlag)
+	labels := flags.FlagToStringToStringPointer(p, cmd, labelsFlag)
+	if labels == nil {
+		labels = &map[string]string{}
+	}
+
+	model := inputModel{
+		GlobalFlagModel: globalFlags,
+		BackupId:        backupId,
+		Name:            name,
+		Labels:          *labels,
+	}
+
+	if p.IsVerbosityDebug() {
+		modelStr, err := print.BuildDebugStrFromInputModel(model)
+		if err != nil {
+			p.Debug(print.ErrorLevel, "convert model to string for debugging: %v", err)
+		} else {
+			p.Debug(print.DebugLevel, "parsed input values: %s", modelStr)
+		}
+	}
+
+	return &model, nil
+}
+
+func buildRequest(ctx context.Context, model *inputModel, apiClient *iaas.APIClient) iaas.ApiUpdateBackupRequest {
+	req := apiClient.UpdateBackup(ctx, model.ProjectId, model.BackupId)
+
+	updatePayload := iaas.NewUpdateBackupPayloadWithDefaults()
+	if model.Name != nil {
+		updatePayload.Name = model.Name
+	}
+
+	// Convert map[string]string to map[string]interface{}
+	var labelsMap *map[string]interface{}
+	if len(model.Labels) > 0 {
+		labelsMap = utils.Ptr(map[string]interface{}{})
+		for k, v := range model.Labels {
+			(*labelsMap)[k] = v
+		}
+	}
+	updatePayload.Labels = labelsMap
+
+	req = req.UpdateBackupPayload(*updatePayload)
+	return req
+}
+
+func outputResult(p *print.Printer, outputFormat, backupLabel string, backup *iaas.Backup) error {
+	if backup == nil {
+		return fmt.Errorf("backup response is empty")
+	}
+
+	switch outputFormat {
+	case print.JSONOutputFormat:
+		details, err := json.MarshalIndent(backup, "", "  ")
+		if err != nil {
+			return fmt.Errorf("marshal backup: %w", err)
+		}
+		p.Outputln(string(details))
+		return nil
+
+	case print.YAMLOutputFormat:
+		details, err := yaml.MarshalWithOptions(backup, yaml.IndentSequence(true), yaml.UseJSONMarshaler())
+		if err != nil {
+			return fmt.Errorf("marshal backup: %w", err)
+		}
+		p.Outputln(string(details))
+		return nil
+
+	default:
+		p.Outputf("Updated backup %q\n", backupLabel)
+		return nil
+	}
+}
