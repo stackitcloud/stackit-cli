fd5cd8528d330d0a40b6283f009a00d53fbf2b63
Benjosh95 <benjamin.fuertsch@inovex.de>
2025-09-10 10:24:24 +0200

    add a proper warning for expired or invalid refresh tokens

--- internal/pkg/auth/auth.go
+++ internal/pkg/auth/auth.go
@@ -202,7 +202,7 @@ func GetValidAccessToken(p *print.Printer) (string, error) {
 	// Refresh the tokens
 	err = refreshTokens(utf)
 	if err != nil {
-		return "", fmt.Errorf("refresh access token: %w", err)
+		return "", fmt.Errorf("access token expired and refresh token is invalid. Please run 'stackit auth login' to re-authenticate: %w", err)
 	}
 
 	// Return the new access token
