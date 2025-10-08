0852adacf572c46cbb3554e5321a2510db44cec2
JanStern <65732422+JanStern@users.noreply.github.com>
2025-10-08 10:07:20 +0200

    Update internal/cmd/beta/kms/wrappingkey/list/list.go
    Co-authored-by: Ruben Hönle <git@hoenle.xyz>

--- internal/cmd/beta/kms/wrappingkey/list/list.go
+++ internal/cmd/beta/kms/wrappingkey/list/list.go
@@ -38,7 +38,7 @@ func NewCmd(params *params.CmdParams) *cobra.Command {
 		Example: examples.Build(
 			examples.NewExample(
 				`List all KMS wrapping keys for the key ring "my-key-ring-id"`,
-				`$ stackit beta kms wrappingkeys list --key-ring "my-key-ring-id"`),
+				`$ stackit beta kms wrapping-key list --key-ring "my-key-ring-id"`),
 			examples.NewExample(
 				`List all KMS wrapping keys in JSON format`,
 				`$ stackit beta kms wrappingkeys list --key-ring "my-key-ring-id" --output-format json`),
