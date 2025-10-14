4b5170ad350cebe131dd01177c2d5158ef2f5533
Jorge Turrado <jorge.turrado@mail.schwarz>
2025-10-14 08:40:07 +0200

    feat: Print auth login during Successfully logged into STACKIT CLI.
    Signed-off-by: Jorge Turrado <jorge.turrado@mail.schwarz>

--- internal/pkg/auth/user_login.go
+++ internal/pkg/auth/user_login.go
@@ -244,6 +244,13 @@ func AuthorizeUser(p *print.Printer, isReauthentication bool) error {
 		return fmt.Errorf("open browser to URL %s: %w", authorizationURL, err)
 	}
 
+	// Print the link
+	p.Outputln("Your browser has been opened to visit:")
+	p.Outputln("")
+	p.Outputf("\t%s\n", authorizationURL)
+	p.Outputln("")
+	p.Outputln("")
+
 	// Start the blocking web server loop
 	// It will exit when the handlers get fired and call server.Close()
 	p.Debug(print.DebugLevel, "listening for response from authentication server on %s", redirectURL)
