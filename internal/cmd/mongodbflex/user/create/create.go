6d9e4b7a32b9f7c82f2fb204c9bc622256080268
Piet van der Meulen <piet.van.der.meulen@freiheit.com>
2025-10-17 12:29:03 +0200

    mongodb-flex: add stackitAdmin role

--- internal/cmd/mongodbflex/user/create/create.go
+++ internal/cmd/mongodbflex/user/create/create.go
@@ -101,7 +101,7 @@ func NewCmd(params *params.CmdParams) *cobra.Command {
 }
 
 func configureFlags(cmd *cobra.Command) {
-	roleOptions := []string{"read", "readWrite", "readWriteAnyDatabase"}
+	roleOptions := []string{"read", "readWrite", "readWriteAnyDatabase", "stackitAdmin"}
 
 	cmd.Flags().Var(flags.UUIDFlag(), instanceIdFlag, "ID of the instance")
 	cmd.Flags().String(usernameFlag, "", "Username of the user. If not specified, a random username will be assigned")
