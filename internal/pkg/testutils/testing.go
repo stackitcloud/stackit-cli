d7bc9504753a13ea0ec670c608f7bf6bb9a28643
Ruben Hoenle <Ruben.Hoenle@stackit.cloud>
2025-10-17 13:19:49 +0200

    initial idea

---
+++ internal/pkg/testutils/testing.go
@@ -0,0 +1,62 @@
+package testutils
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/spf13/cobra"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/params"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/globalflags"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/print"
+)
+
+func TestParseInput[T any](t *testing.T, cmdFactory func(*params.CmdParams) *cobra.Command, parseInputFunc func(*print.Printer, *cobra.Command, []string) (T, error), expectedModel T, argValues []string, flagValues map[string]string, isValid bool) {
+	p := print.NewPrinter()
+	cmd := cmdFactory(&params.CmdParams{Printer: p})
+	err := globalflags.Configure(cmd.Flags())
+	if err != nil {
+		t.Fatalf("configure global flags: %v", err)
+	}
+
+	for flag, value := range flagValues {
+		err := cmd.Flags().Set(flag, value)
+		if err != nil {
+			if !isValid {
+				return
+			}
+			t.Fatalf("setting flag --%s=%s: %v", flag, value, err)
+		}
+	}
+
+	err = cmd.ValidateArgs(argValues)
+	if err != nil {
+		if !isValid {
+			return
+		}
+		t.Fatalf("error validating args: %v", err)
+	}
+
+	err = cmd.ValidateRequiredFlags()
+	if err != nil {
+		if !isValid {
+			return
+		}
+		t.Fatalf("error validating flags: %v", err)
+	}
+
+	model, err := parseInputFunc(p, cmd, argValues)
+	if err != nil {
+		if !isValid {
+			return
+		}
+		t.Fatalf("error parsing input: %v", err)
+	}
+
+	if !isValid {
+		t.Fatalf("did not fail on invalid input")
+	}
+	diff := cmp.Diff(model, expectedModel)
+	if diff != "" {
+		t.Fatalf("Data does not match: %s", diff)
+	}
+}
