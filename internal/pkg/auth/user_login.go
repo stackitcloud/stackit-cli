512c6ee39e285e197f42fed019fe64cdb8aa97bd
Jorge Turrado <jorge.turrado@mail.schwarz>
2025-10-17 13:29:23 +0200

    update message
    Signed-off-by: Jorge Turrado <jorge.turrado@mail.schwarz>

--- internal/pkg/auth/user_login.go
+++ internal/pkg/auth/user_login.go
@@ -245,11 +245,8 @@ func AuthorizeUser(p *print.Printer, isReauthentication bool) error {
 	}
 
 	// Print the link
-	p.Outputln("Your browser has been opened to visit:")
-	p.Outputln("")
-	p.Outputf("\t%s\n", authorizationURL)
-	p.Outputln("")
-	p.Outputln("")
+	p.Outputln("Your browser has been opened to visit:\n")
+	p.Outputf("\t%s\n\n", authorizationURL)
 
 	// Start the blocking web server loop
 	// It will exit when the handlers get fired and call server.Close()
