44016879d17ece917e7c8c2f7fae92766ea10298
Piet van der Meulen <piet.van.der.meulen@freiheit.com>
2025-10-22 16:05:56 +0200

    add readAnyDatabase role

--- internal/cmd/mongodbflex/user/update/update.go
+++ internal/cmd/mongodbflex/user/update/update.go
@@ -97,7 +97,7 @@ func NewCmd(params *params.CmdParams) *cobra.Command {
 }
 
 func configureFlags(cmd *cobra.Command) {
-	roleOptions := []string{"read", "readWrite", "readWriteAnyDatabase", "stackitAdmin"}
+	roleOptions := []string{"read", "readWrite", "readAnyDatabase", "readWriteAnyDatabase", "stackitAdmin"}
 
 	cmd.Flags().Var(flags.UUIDFlag(), instanceIdFlag, "ID of the instance")
 	cmd.Flags().String(databaseFlag, "", "The database inside the MongoDB instance that the user has access to. If it does not exist, it will be created once the user writes to it")
