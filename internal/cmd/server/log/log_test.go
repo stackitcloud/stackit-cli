e5375ed19e2db9e4ab9fa69ea74ff1d796a5ba8c
Mauritz Uphoff <mauritz.uphoff@stackit.cloud>
2025-10-23 09:38:41 +0200

    feat(logs): improve output for server logs

--- internal/cmd/server/log/log_test.go
+++ internal/cmd/server/log/log_test.go
@@ -178,9 +178,8 @@ func TestBuildRequest(t *testing.T) {
 
 func TestOutputResult(t *testing.T) {
 	type args struct {
-		outputFormat string
-		serverLabel  string
-		log          string
+		serverLabel string
+		logLines    []string
 	}
 	tests := []struct {
 		name    string
@@ -197,7 +196,7 @@ func TestOutputResult(t *testing.T) {
 	p.Cmd = NewCmd(&params.CmdParams{Printer: p})
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := outputResult(p, tt.args.outputFormat, tt.args.serverLabel, tt.args.log); (err != nil) != tt.wantErr {
+			if err := outputResult(p, tt.args.serverLabel, tt.args.logLines); (err != nil) != tt.wantErr {
 				t.Errorf("outputResult() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
