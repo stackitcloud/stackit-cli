0493784f8f6c5bb7bd6347d59b0a06050bc9a929
Mauritz Uphoff <mauritz.uphoff@stackit.cloud>
2025-10-27 14:27:29 +0100

    review changes

---
+++ docs/stackit_routing-table_delete.md
@@ -0,0 +1,42 @@
+## stackit routing-table delete
+
+Deletes a routing-table
+
+### Synopsis
+
+Deletes a routing-table
+
+```
+stackit routing-table delete ROUTING_TABLE_ARG [flags]
+```
+
+### Examples
+
+```
+  Deletes a a routing-table
+  $ stackit routing-table delete xxxx-xxxx-xxxx-xxxx --organization-id yyy --network-area-id zzz
+```
+
+### Options
+
+```
+  -h, --help                     Help for "stackit routing-table delete"
+      --network-area-id string   Network-Area ID
+      --organization-id string   Organization ID
+```
+
+### Options inherited from parent commands
+
+```
+  -y, --assume-yes             If set, skips all confirmation prompts
+      --async                  If set, runs the command asynchronously
+  -o, --output-format string   Output format, one of ["json" "pretty" "none" "yaml"]
+  -p, --project-id string      Project ID
+      --region string          Target region for region-specific requests
+      --verbosity string       Verbosity of the CLI, one of ["debug" "info" "warning" "error"] (default "info")
+```
+
+### SEE ALSO
+
+* [stackit routing-table](./stackit_routing-table.md)	 - Manage routing-tables and its according routes
+
---
+++ internal/cmd/routingtable/route/update/update_test.go
@@ -0,0 +1,211 @@
+package update
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/params"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/globalflags"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/print"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/testutils"
+	"github.com/stackitcloud/stackit-cli/internal/pkg/utils"
+	"github.com/stackitcloud/stackit-sdk-go/services/iaas"
+)
+
+const testRegion = "eu01"
+
+var testOrgId = uuid.NewString()
+var testNetworkAreaId = uuid.NewString()
+var testRoutingTableId = uuid.NewString()
+var testRouteId = uuid.NewString()
+
+const testLabelSelectorFlag = "key1=value1,key2=value2"
+
+var testLabels = &map[string]string{
+	"key1": "value1",
+	"key2": "value2",
+}
+
+func fixtureFlagValues(mods ...func(flagValues map[string]string)) map[string]string {
+	flagValues := map[string]string{
+		globalflags.RegionFlag: testRegion,
+		organizationIdFlag:     testOrgId,
+		networkAreaIdFlag:      testNetworkAreaId,
+		routingTableIdFlag:     testRoutingTableId,
+		labelFlag:              testLabelSelectorFlag,
+	}
+	for _, mod := range mods {
+		mod(flagValues)
+	}
+	return flagValues
+}
+
+func fixtureInputModel(mods ...func(model *inputModel)) *inputModel {
+	model := &inputModel{
+		GlobalFlagModel: &globalflags.GlobalFlagModel{
+			Verbosity: globalflags.VerbosityDefault,
+			Region:    testRegion,
+		},
+		OrganizationId: utils.Ptr(testOrgId),
+		NetworkAreaId:  utils.Ptr(testNetworkAreaId),
+		RoutingTableId: utils.Ptr(testRoutingTableId),
+		RouteId:        &testRouteId,
+		Labels:         testLabels,
+	}
+	for _, mod := range mods {
+		mod(model)
+	}
+	return model
+}
+
+func fixtureArgValues(mods ...func(argValues []string)) []string {
+	argValues := []string{
+		testRouteId,
+	}
+	for _, mod := range mods {
+		mod(argValues)
+	}
+	return argValues
+}
+
+func TestParseInput(t *testing.T) {
+	tests := []struct {
+		description   string
+		flagValues    map[string]string
+		argValues     []string
+		isValid       bool
+		expectedModel *inputModel
+	}{
+		{
+			description:   "base",
+			flagValues:    fixtureFlagValues(),
+			argValues:     fixtureArgValues(),
+			isValid:       true,
+			expectedModel: fixtureInputModel(),
+		},
+		{
+			description: "no values",
+			argValues:   []string{},
+			flagValues:  map[string]string{},
+			isValid:     false,
+		},
+		{
+			description: "routing-table-id missing",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				delete(flagValues, routingTableIdFlag)
+			}),
+			isValid: false,
+		},
+		{
+			description: "network-area-id missing",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				delete(flagValues, networkAreaIdFlag)
+			}),
+			isValid: false,
+		},
+		{
+			description: "org-id missing",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				delete(flagValues, organizationIdFlag)
+			}),
+			isValid: false,
+		},
+		{
+			description: "routing-table-id missing",
+			argValues:   fixtureArgValues(),
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				delete(flagValues, routingTableIdFlag)
+			}),
+			isValid: false,
+		},
+		{
+			description:   "arg value missing",
+			argValues:     []string{""},
+			flagValues:    fixtureFlagValues(),
+			isValid:       false,
+			expectedModel: fixtureInputModel(),
+		},
+		{
+			description:   "arg value wrong",
+			argValues:     []string{"foo-bar"},
+			flagValues:    fixtureFlagValues(),
+			isValid:       false,
+			expectedModel: fixtureInputModel(),
+		},
+		{
+			description: "labels are missing",
+			argValues:   []string{},
+			flagValues: fixtureFlagValues(func(flagValues map[string]string) {
+				delete(flagValues, labelFlag)
+			}),
+			isValid: false,
+		},
+		{
+			description: "invalid label format",
+			argValues:   []string{},
+			flagValues:  map[string]string{labelFlag: "invalid-label"},
+			isValid:     false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			testutils.TestParseInput(t, NewCmd, parseInput, tt.expectedModel, tt.argValues, tt.flagValues, tt.isValid)
+		})
+	}
+}
+
+func TestOutputResult(t *testing.T) {
+	dummyRoute := iaas.Route{
+		Id: utils.Ptr("route-foo"),
+		Destination: &iaas.RouteDestination{
+			DestinationCIDRv4: &iaas.DestinationCIDRv4{
+				Type:  utils.Ptr("cidrv4"),
+				Value: utils.Ptr("10.0.0.0/24"),
+			},
+		},
+		Nexthop: &iaas.RouteNexthop{
+			NexthopIPv4: &iaas.NexthopIPv4{
+				Type:  utils.Ptr("ipv4"),
+				Value: utils.Ptr("10.0.0.1"),
+			},
+		},
+		Labels:    utils.ConvertStringMapToInterfaceMap(testLabels),
+		CreatedAt: utils.Ptr(time.Now()),
+		UpdatedAt: utils.Ptr(time.Now()),
+	}
+
+	tests := []struct {
+		name         string
+		outputFormat string
+		route        iaas.Route
+		wantErr      bool
+	}{
+		{
+			name:         "json output with one route",
+			outputFormat: print.JSONOutputFormat,
+			route:        dummyRoute,
+			wantErr:      false,
+		},
+		{
+			name:         "yaml output with one route",
+			outputFormat: print.YAMLOutputFormat,
+			route:        dummyRoute,
+			wantErr:      false,
+		},
+	}
+
+	p := print.NewPrinter()
+	p.Cmd = NewCmd(&params.CmdParams{Printer: p})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := outputResult(p, tt.outputFormat, "", "", &tt.route); (err != nil) != tt.wantErr {
+				t.Errorf("outputResult() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
