ccfd45ae7d5aefab4a1e87f0237729ffb0b9bf5f
Devansh Thakur <devansh.thakur@mail.schwarz>
2025-11-26 15:40:55 +0100

    intake component on STACKIT cli

--- internal/cmd/beta/intake/intake.go
+++ internal/cmd/beta/intake/intake.go
@@ -2,6 +2,11 @@ package intake
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/intake/instance/create"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/intake/instance/delete"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/intake/instance/describe"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/intake/instance/list"
+	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/intake/instance/update"
 	"github.com/stackitcloud/stackit-cli/internal/cmd/beta/intake/runner"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/args"
 	"github.com/stackitcloud/stackit-cli/internal/pkg/types"
@@ -23,4 +28,11 @@ func NewCmd(params *types.CmdParams) *cobra.Command {
 
 func addSubcommands(cmd *cobra.Command, params *types.CmdParams) {
 	cmd.AddCommand(runner.NewCmd(params))
+
+	// Intake instance subcommands
+	cmd.AddCommand(create.NewCmd(params))
+	cmd.AddCommand(describe.NewCmd(params))
+	cmd.AddCommand(list.NewCmd(params))
+	cmd.AddCommand(update.NewCmd(params))
+	cmd.AddCommand(delete.NewCmd(params))
 }
