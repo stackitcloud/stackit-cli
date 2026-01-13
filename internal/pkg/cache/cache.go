0b3de3959d0ffcdb2c6a0615031d223f7de143fe
Michael Eischer <michael.eischer@inovex.de>
2026-01-13 15:55:58 +0100

    ske kubeconfig login: regularly rotate cache encryption key

--- internal/pkg/cache/cache.go
+++ internal/pkg/cache/cache.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
+	"time"
 
 	"github.com/stackitcloud/stackit-cli/internal/pkg/auth"
 )
@@ -22,6 +24,10 @@ var (
 	ErrorInvalidCacheIdentifier = fmt.Errorf("invalid cache identifier")
 )
 
+const (
+	cacheKeyMaxAge = 90 * 24 * time.Hour
+)
+
 func Init() error {
 	cacheDir, err := os.UserCacheDir()
 	if err != nil {
@@ -29,9 +35,19 @@ func Init() error {
 	}
 	cacheFolderPath = filepath.Join(cacheDir, "stackit")
 
+	// Encryption keys should only be used a limited number of times for aes-gcm.
+	// Thus, refresh the key periodically. This will invalidate all cached entries.
 	key, _ := auth.GetAuthField(auth.CACHE_ENCRYPTION_KEY)
+	age, _ := auth.GetAuthField(auth.CACHE_ENCRYPTION_KEY_AGE)
 	cacheEncryptionKey = nil
-	if key != "" {
+	var keyAge time.Time
+	if age != "" {
+		ageSeconds, err := strconv.ParseInt(age, 10, 64)
+		if err == nil {
+			keyAge = time.Unix(ageSeconds, 0)
+		}
+	}
+	if key != "" && keyAge.Add(cacheKeyMaxAge).After(time.Now()) {
 		cacheEncryptionKey, _ = base64.StdEncoding.DecodeString(key)
 		// invalid key length
 		if len(cacheEncryptionKey) != 32 {
@@ -45,7 +61,14 @@ func Init() error {
 			return fmt.Errorf("cache encryption key: %v", err)
 		}
 		key := base64.StdEncoding.EncodeToString(cacheEncryptionKey)
-		return auth.SetAuthField(auth.CACHE_ENCRYPTION_KEY, key)
+		err = auth.SetAuthField(auth.CACHE_ENCRYPTION_KEY, key)
+		if err != nil {
+			return fmt.Errorf("save cache encryption key: %v", err)
+		}
+		err = auth.SetAuthField(auth.CACHE_ENCRYPTION_KEY_AGE, fmt.Sprint(time.Now().Unix()))
+		if err != nil {
+			return fmt.Errorf("save cache encryption key age: %v", err)
+		}
 	}
 	return nil
 }
