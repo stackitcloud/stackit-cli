099cb5ff62f9fe523b2acd8f692e94c575f35661
Maximilian Geberl <maximilian.geberl@stackit.cloud>
2026-01-15 09:38:27 +0100

    Fix typo

--- internal/pkg/config/profiles.go
+++ internal/pkg/config/profiles.go
@@ -80,7 +80,7 @@ func GetProfileFromEnv() (string, bool) {
 // If emptyProfile is true, it creates an empty profile. Otherwise, copies the config from the current profile to the new profile.
 // If setProfile is true, it sets the new profile as the active profile.
 // If the profile already exists and ignoreExisting is false, it returns an error.
-func CreateProfile(p *print.Printer, profile string, setProfile, ignoreExising, emptyProfile bool) error {
+func CreateProfile(p *print.Printer, profile string, setProfile, ignoreExisting, emptyProfile bool) error {
 	err := ValidateProfile(profile)
 	if err != nil {
 		return fmt.Errorf("validate profile: %w", err)
@@ -98,7 +98,7 @@ func CreateProfile(p *print.Printer, profile string, setProfile, ignoreExising, 
 	// Error if the profile already exists
 	_, err = os.Stat(configFolderPath)
 	if err == nil {
-		if ignoreExising {
+		if ignoreExisting {
 			if setProfile {
 				err = SetProfile(p, profile)
 				if err != nil {
