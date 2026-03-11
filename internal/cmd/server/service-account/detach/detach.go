0b406a096a7dce0edc1b8720d40542169bc064a5
Inter, Sven <Sven.Inter@opitz-consulting.com>
2026-03-11 09:52:14 +0100

    fix(server): fix typo

--- internal/cmd/server/service-account/detach/detach.go
+++ internal/cmd/server/service-account/detach/detach.go
@@ -63,7 +63,7 @@ func NewCmd(params *types.CmdParams) *cobra.Command {
 				serverLabel = model.ServerId
 			}
 
-			prompt := fmt.Sprintf("Are your sure you want to detach service account %q from a server %q?", model.ServiceAccMail, serverLabel)
+			prompt := fmt.Sprintf("Are you sure you want to detach service account %q from a server %q?", model.ServiceAccMail, serverLabel)
 			err = params.Printer.PromptForConfirmation(prompt)
 			if err != nil {
 				return err
