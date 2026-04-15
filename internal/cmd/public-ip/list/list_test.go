4661dc56c65615472722f33dbefdf1d99420a8e9
Jan Obernberger <jan.obernberger@digits.schwarz>
2026-04-15 13:01:25 +0200

    adapted public ip list command to align to expectations

--- internal/cmd/public-ip/list/list_test.go
+++ internal/cmd/public-ip/list/list_test.go
@@ -174,6 +174,7 @@ func TestBuildRequest(t *testing.T) {
 func TestOutputResult(t *testing.T) {
 	type args struct {
 		outputFormat string
+		projectLabel string
 		publicIps    []iaas.PublicIp
 	}
 	tests := []struct {
@@ -190,7 +191,7 @@ func TestOutputResult(t *testing.T) {
 	params := testparams.NewTestParams()
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := outputResult(params.Printer, tt.args.outputFormat, tt.args.publicIps); (err != nil) != tt.wantErr {
+			if err := outputResult(params.Printer, tt.args.outputFormat, tt.args.projectLabel, tt.args.publicIps); (err != nil) != tt.wantErr {
 				t.Errorf("outputResult() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
