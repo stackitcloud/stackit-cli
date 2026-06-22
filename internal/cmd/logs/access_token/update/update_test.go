242e57444c479a339f1a954a5e9fed3741419ba1
Jan Obernberger <jan.obernberger@digits.schwarz>
2026-06-22 16:38:45 +0200

    chore(logs): refactor access token update

--- internal/cmd/logs/access_token/update/update_test.go
+++ internal/cmd/logs/access_token/update/update_test.go
@@ -25,7 +25,7 @@ type testCtxKey struct{}
 
 var (
 	testCtx    = context.WithValue(context.Background(), testCtxKey{}, "foo")
-	testClient = &logs.APIClient{}
+	testClient = &logs.APIClient{DefaultAPI: &logs.DefaultAPIService{}}
 
 	testProjectId     = uuid.NewString()
 	testInstanceId    = uuid.NewString()
@@ -77,7 +77,7 @@ func fixtureInputModel(mods ...func(model *inputModel)) *inputModel {
 }
 
 func fixtureRequest(mods ...func(request *logs.ApiUpdateAccessTokenRequest)) logs.ApiUpdateAccessTokenRequest {
-	request := testClient.UpdateAccessToken(testCtx, testProjectId, testRegion, testInstanceId, testAccessTokenId)
+	request := testClient.DefaultAPI.UpdateAccessToken(testCtx, testProjectId, testRegion, testInstanceId, testAccessTokenId)
 	request = request.UpdateAccessTokenPayload(fixturePayload())
 	for _, mod := range mods {
 		mod(&request)
@@ -266,6 +266,7 @@ func TestBuildRequest(t *testing.T) {
 			diff := cmp.Diff(request, tt.expectedRequest,
 				cmp.AllowUnexported(tt.expectedRequest),
 				cmpopts.EquateComparable(testCtx),
+				cmpopts.IgnoreFields(tt.expectedRequest, "ApiService"),
 			)
 			if diff != "" {
 				t.Fatalf("Data does not match: %s", diff)
